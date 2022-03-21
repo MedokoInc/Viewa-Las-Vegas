eb74f1a0c356803d78d5b504728858a733039f09
myBernhard <92299433+myBernhard@users.noreply.github.com>
2022-03-21 10:35:06 +0100

    Composer added

--- src/Hotel.php
+++ src/Hotel.php
@@ -8,12 +8,14 @@ class Hotel
     private $name;
     private $description;
     private $img;
+    private $url;
 
-    public function __construct($name, $description, $img)
+    public function __construct($name, $description, $img, $url)
     {
         $this->name = $name;
         $this->description = $description;
         $this->img = $img;
+        $this->url = $url;
     }
 
     /**
@@ -38,5 +40,18 @@ class Hotel
     public function getImg()
     {
         return $this->img;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    public function getArray()
+    {
+        return ['NAME'=>$this->name,'DESCRIPTION'=>$this->description,'IMG'=>$this->img,'URL'=>$this->url];
     }
 }
